8cd658ade1831fad49bcff9d0764d75d02fe3e5b
tla <tla@mit.edu>
2015-11-17 20:27:22 +0100

    add a call to query the database

--- ppres/routes/index.js
+++ ppres/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+/* Initialize our SQLite database. */
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database("slideshows.db");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/listslideshows', function(req, res, next) {
+	db.all("SELECT id, name FROM slideshows", function(err, rows) {
+		res.send(rows);
+	});
 });
 
 module.exports = router;
